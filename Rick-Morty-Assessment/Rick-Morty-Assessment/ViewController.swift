907d3c1ade008bb4bc20bd73eee18059e24e2fc4
Ege Seçkin <ege.seckin@outlook.com>
2022-06-24 18:41:00 +0300

    project created

---
+++ Rick-Morty-Assessment/Rick-Morty-Assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick-Morty-Assessment
+//
+//  Created by Ege Seçkin on 24.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
